7f766e4b50245a3f6d1b04c04f96f1d89f53743a
Jeroen Leenarts <jeroen.leenarts@gmail.com>
2023-12-11 22:20:39 +0100

    Add initial project.

---
+++ ios-livestreamer-innercircle/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ios-livestreamer-innercircle
+//
+//  Created by Jeroen Leenarts on 11/12/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ios-livestreamer-innercircle/ios_livestreamer_innercircleApp.swift
@@ -0,0 +1,17 @@
+//
+//  ios_livestreamer_innercircleApp.swift
+//  ios-livestreamer-innercircle
+//
+//  Created by Jeroen Leenarts on 11/12/2023.
+//
+
+import SwiftUI
+
+@main
+struct ios_livestreamer_innercircleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
