f7db2edd5d91314bf5b7d5db590dc2859a9541bf
Mikaela Caron <mikaelacaron@gmail.com>
2023-12-21 17:22:01 -0500

    Create LivestreamView

---
+++ ios-livestreamer-innercircle/LivestreamView.swift
@@ -0,0 +1,18 @@
+//
+//  LivestreamView.swift
+//  ios-livestreamer-innercircle
+//
+//  Created by Mikaela Caron on 12/21/23.
+//
+
+import SwiftUI
+
+struct LivestreamView: View {
+    var body: some View {
+        Text("Livestream View")
+    }
+}
+
+#Preview {
+    LivestreamView()
+}
