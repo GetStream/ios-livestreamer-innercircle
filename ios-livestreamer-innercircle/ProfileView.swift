f2a543da467dd383b40d3365ffb5e25ddb0b7cb1
Mikaela Caron <mikaelacaron@gmail.com>
2023-12-21 17:40:03 -0500

    Add ProfileView

---
+++ ios-livestreamer-innercircle/ProfileView.swift
@@ -0,0 +1,18 @@
+//
+//  ProfileView.swift
+//  ios-livestreamer-innercircle
+//
+//  Created by Mikaela Caron on 12/21/23.
+//
+
+import SwiftUI
+
+struct ProfileView: View {
+    var body: some View {
+        Text("Profile View")
+    }
+}
+
+#Preview {
+    ProfileView()
+}
