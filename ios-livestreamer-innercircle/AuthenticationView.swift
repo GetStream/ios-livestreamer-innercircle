3f2485acb2e8c7c7c005e75e43cd7cf6df657293
Mikaela Caron <mikaelacaron@gmail.com>
2023-12-21 16:57:25 -0500

    Add AuthenticationView

---
+++ ios-livestreamer-innercircle/AuthenticationView.swift
@@ -0,0 +1,18 @@
+//
+//  AuthenticationView.swift
+//  ios-livestreamer-innercircle
+//
+//  Created by Mikaela Caron on 12/21/23.
+//
+
+import SwiftUI
+
+struct AuthenticationView: View {
+    var body: some View {
+        Text("AuthenticationView")
+    }
+}
+
+#Preview {
+    AuthenticationView()
+}
