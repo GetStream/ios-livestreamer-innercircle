63537a0af23dce5cef7b2a3a6b2ba4a35818c550
Mikaela Caron <mikaelacaron@gmail.com>
2023-12-21 17:15:46 -0500

    Rename to HomeView

--- ios-livestreamer-innercircle/HomeView.swift
+++ ios-livestreamer-innercircle/HomeView.swift
@@ -1,5 +1,5 @@
 //
-//  ContentView.swift
+//  HomeView.swift
 //  ios-livestreamer-innercircle
 //
 //  Created by Jeroen Leenarts on 11/12/2023.
@@ -7,7 +7,7 @@
 
 import SwiftUI
 
-struct ContentView: View {
+struct HomeView: View {
     var body: some View {
         VStack {
             Image(systemName: "globe")
@@ -20,5 +20,5 @@ struct ContentView: View {
 }
 
 #Preview {
-    ContentView()
+    HomeView()
 }
--- ios-livestreamer-innercircle/ios_livestreamer_innercircleApp.swift
+++ ios-livestreamer-innercircle/ios_livestreamer_innercircleApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct ios_livestreamer_innercircleApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            HomeView()
         }
     }
 }
