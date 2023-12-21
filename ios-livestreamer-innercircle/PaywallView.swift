0fb066468edd27da0dae61088205cabe3dda7282
Mikaela Caron <mikaelacaron@gmail.com>
2023-12-21 17:21:38 -0500

    Create PaywallView

---
+++ ios-livestreamer-innercircle/PaywallView.swift
@@ -0,0 +1,18 @@
+//
+//  PaywallView.swift
+//  ios-livestreamer-innercircle
+//
+//  Created by Mikaela Caron on 12/21/23.
+//
+
+import SwiftUI
+
+struct PaywallView: View {
+    var body: some View {
+        Text("Paywall View")
+    }
+}
+
+#Preview {
+    PaywallView()
+}
