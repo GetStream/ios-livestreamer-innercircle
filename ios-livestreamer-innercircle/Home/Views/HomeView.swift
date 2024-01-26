c4eb06979ce17d9a6d49788d504c5d6d43ee2e64
Mikaela Caron <mikaelacaron@gmail.com>
2024-01-26 07:08:52 -0500

    Initializing StreamVideo
    This is not set up properly yet

--- ios-livestreamer-innercircle/Home/Views/HomeView.swift
+++ ios-livestreamer-innercircle/Home/Views/HomeView.swift
@@ -6,12 +6,29 @@
 //
 
 import SwiftUI
+import StreamVideo
 
 struct HomeView: View {
     
     @Environment(AuthenticationViewModel.self) var authenticationViewModel
     
     @State private var isPresentingProfileView = false
+    
+    @State private var streamVideoClient: StreamVideo
+    
+    @State private var calls = [Call]()
+    
+    init() {
+//        let userToken = (try? await authenticationViewModel.user?.getIDToken()) ?? ""
+        
+        let streamVideo = StreamVideo(
+            apiKey: "",
+            user: .anonymous,
+            token: .init(rawValue: "userToken")
+        )
+        
+        self.streamVideoClient = streamVideo
+    }
     
     private let livestreams = ["video 1", "video 2", "video 3"]
     
@@ -40,11 +57,25 @@ struct HomeView: View {
                     }
                 }
             }
+            .task {
+                try? await queryLivestreams()
+            }
             .sheet(isPresented: $isPresentingProfileView) {
                 ProfileView()
                     .environment(authenticationViewModel)
             }
         }
+    }
+    
+    private func queryLivestreams() async throws {
+//        let filters: [String: RawJSON] = ["ended_at": .nil]
+        let filters: [String: RawJSON] = ["type": .dictionary(["$eq": .string("livestream")])]
+        let sort = [SortParamRequest.descending("created_at")]
+        let limit = 10
+        
+        let (firstPageCalls, secondPageCursor) = try await streamVideoClient.queryCalls(filters: filters, sort: sort, limit: limit)
+        
+        self.calls = firstPageCalls
     }
 }
 
